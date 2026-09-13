2091ce400ea62cba612fd1ff548a91b5810434e3
123alfi <ugcscsonacasalfiya@gmail.com>
2026-09-13 16:08:28 +0530

    modified five operators_C programs

--- operators_C/08_last_digit.c
+++ operators_C/08_last_digit.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+int main() {
+    int n, last_digit;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    last_digit = n % 10;
+
+    printf("Last digit = %d", last_digit);
+
+    return 0;
+}
