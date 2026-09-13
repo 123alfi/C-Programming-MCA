9d0595ee3a96520d7bbfb73063918d02b5757086
123alfi <ugcscsonacasalfiya@gmail.com>
2026-09-13 16:21:05 +0530

    modified five decision_making_C programs

--- decision_making_C/14_grade.c
+++ decision_making_C/14_grade.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
 
+int main() {
+    int marks;
+
+    printf("Enter marks: ");
+    scanf("%d", &marks);
+
+    if (marks >= 90 && marks <= 100) {
+        printf("Grade A");
+    } else if (marks >= 80 && marks <= 89) {
+        printf("Grade B");
+    } else if (marks >= 70 && marks <= 79) {
+        printf("Grade C");
+    } else if (marks >= 60 && marks <= 69) {
+        printf("Grade D");
+    } else if (marks < 60 && marks >= 0) {
+        printf("Grade F");
+    } else {
+        printf("Invalid marks");
+    }
+
+    return 0;
+}
--- decision_making_C/15_calculator.c
+++ decision_making_C/15_calculator.c
@@ -1,1 +1,35 @@
+#include <stdio.h>
 
+int main() {
+    int a, b;
+    char operator;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    printf("Enter operator (+, -, *, /): ");
+    scanf(" %c", &operator);
+
+    switch (operator) {
+        case '+':
+            printf("Result = %d", a + b);
+            break;
+
+        case '-':
+            printf("Result = %d", a - b);
+            break;
+
+        case '*':
+            printf("Result = %d", a * b);
+            break;
+
+        case '/':
+            printf("Result = %d", a / b);
+            break;
+
+        default:
+            printf("Invalid operator");
+    }
+
+    return 0;
+}
