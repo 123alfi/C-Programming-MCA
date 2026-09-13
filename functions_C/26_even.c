1331f5898a463d47ecc48971275d8876d75e588d
123alfi <ugcscsonacasalfiya@gmail.com>
2026-09-13 16:34:59 +0530

    modified six functions_C program

--- functions_C/26_even.c
+++ functions_C/26_even.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int even(int n) {
+    return n % 2 == 0;
+}
+
+int main() {
+    int n;
+
+    scanf("%d", &n);
+
+    if(even(n))
+        printf("Even");
+    else
+        printf("Odd");
+
+    return 0;
+}
